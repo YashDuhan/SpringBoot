089dba5dc3b5155583a7dd3d043e122fb6de605b
Yash Duhan <70028651+YashDuhan@users.noreply.github.com>
2024-08-09 06:16:19 +0530

    1st Commit

---
+++ FirstLec/src/test/java/com/firstLec/FirstLec/FirstLecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstLec.FirstLec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstLecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
